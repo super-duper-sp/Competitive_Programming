83bff9513b0ca92c92ca9631fd38aa45e1ab9f39
Shubham Patidar <68178609+super-duper-sp@users.noreply.github.com>
2021-09-01 23:42:54 +0530

    Add files via upload

---
+++ CodeChef/ATM(Add To Numbers).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    int n,i,a,b,sum;
+    cin>>n;
+	for(i=0;i<n;i++)
+	{
+	    cin>> a >> b ;
+	    sum = a+b;
+	    cout<< sum << endl;
+	    
+	    
+	}
+return 0;
+}
